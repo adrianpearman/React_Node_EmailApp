d1280ae4f776c9858c5afe1728a2520f07fd5e38
Adrian Pearman <adrianpearman12@gmail.com>
2017-10-18 17:48:07 -0400

    comments added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/',(req,res) => {
-  res.send({hi: there})
+
+app.get('/', (req, res) => {
+  res.send({ hi: 'there'})
 })
 
+// this below code will allow the code to be run on either heroku or locally
 const PORT = process.env.port || 5000
-
+// The application is being made available on localhost:5000
 app.listen(PORT)
