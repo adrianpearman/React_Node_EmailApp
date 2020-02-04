818f8511e8446b28ae3a23ab92b4969c4f3c3c21
Adrian Pearman <adrianpearman12@gmail.com>
2020-02-04 18:20:51 -0500

    updated gitignore to include build folder.

---
+++ client/build/precache-manifest.095c5caf947fc37397e6b7f012958a00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "423cf177f09c8b93aec8a08367695164",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5513f06d557c062adef0",
+    "url": "/static/css/2.2623327d.chunk.css"
+  },
+  {
+    "revision": "5513f06d557c062adef0",
+    "url": "/static/js/2.6334c438.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/static/js/2.6334c438.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0989b1ef92db79c784",
+    "url": "/static/js/main.7f1bd1ad.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
