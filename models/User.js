7640a3eee6c01d2ee46fedce14627e90ce516908
Adrian Pearman <adrianpearman12@gmail.com>
2020-02-04 18:32:30 -0500

    readded User model.

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+// const Schema = mongoose.Schema; is un destructured version of the code below
+const { Schema } = mongoose;
+
+// the Schema object describes what each individual record will look like
+const userSchema = new Schema({
+  googleID: String,
+  // to add a default value, we create an object. default value is where the value begins
+  credits: { type: Number, default: 0 }
+});
+// adds a new collection called 'users' with the information from userSchema
+mongoose.model("users", userSchema);
